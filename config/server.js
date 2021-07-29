6c5017434738ba680d660fa989547527399fb9f9
Ramanan17 <ramanan.b@getkitsune.com>
2021-07-29 09:27:57 +0530

    Initial_Models

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2721d550af14b7eed12117ada4478975'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1182af20-7ba9-4d0b-8e3d-0c5a7716b8d4'
+};
